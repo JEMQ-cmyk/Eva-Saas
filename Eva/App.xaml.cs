7883ef770c46c3bcc37dc49b156898917d2186d3
João Eudes Marques <joaoeudes.eudesmar@gmail.com>
2026-07-09 15:24:32 -0300

    Adicionar arquivos de projeto.

---
+++ Eva/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Eva
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
